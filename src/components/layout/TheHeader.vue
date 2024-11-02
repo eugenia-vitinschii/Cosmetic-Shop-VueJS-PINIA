d3c3189a0d616f5c10ba0589e1a056536b2c7c43
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2024-11-02 16:21:04 +0200

    add sass styles, edit components

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -5,35 +5,9 @@
         <nav>
           <ul class="header__top">
             <!-- home -->
-            <li class="header__link">
-              <router-link  to="/">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                  fill="#e8eaed"
-                >
-                  <path
-                    d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"
-                  />
-                </svg>
-              </router-link>
-            </li>
-            <!-- favorite -->
             <li class="header__link"> 
               <router-link to="/">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                  fill="#e8eaed"
-                >
-                  <path
-                    d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"
-                  />
-                </svg>
+              Home
               </router-link>
             </li>
             <!-- brands -->
@@ -42,10 +16,12 @@
             </li>
             <!-- category -->
             <li class="header__link dropdown"> 
-              <router-link to="/all-category"> Category </router-link>
+              <a  @click="showCategory = ! showCategory">
+               Category
+              </a>
             </li>
             <!-- category items -->
-            <ul class="header__bottom">
+            <ul class="header__bottom" v-show="showCategory">
               <li class="header__link">
                 <router-link to="/category/liquid">LIQUID</router-link>
               </li>
@@ -88,9 +64,11 @@
             </ul>
             <!-- type -->
             <li class="header__link dropdown">
-              <router-link   to="/all-product-type">Types</router-link>
+              <a @click="showType = ! showType">
+               Types
+              </a>
             </li>
-            <ul class="header__bottom">
+            <ul class="header__bottom" v-show="showType">
                <li class="header__link">
                 <router-link to="/product-type/foundation" >FOUNDATION </router-link>
               </li>
@@ -122,9 +100,12 @@
 
 
 <script setup>
-import { defineOptions } from "vue";
+import { defineOptions, ref} from "vue";
 
 defineOptions({
   name: "TheHeader",
 });
+
+const showCategory = ref(false)
+const showType= ref(false)
 </script>

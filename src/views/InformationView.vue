724d95136db259cf795a40f5cdfb008ef4b7f0a5
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2024-11-09 18:31:13 +0200

    create small nav item component, create html for information view

--- src/views/InformationView.vue
+++ src/views/InformationView.vue
@@ -4,22 +4,81 @@
       <div class="info__wrapper">
          <div class="info__nav">
             <ul>
-               <li><a href="#about">About</a></li>
-               <li><a href="#contacts">Contacts</a></li>
-               <li><a href="#terms">Terms</a></li>
-               <li><a href="#privacy-policy">privacy policy</a></li>
+               <small-nav-item 
+               v-for="item in nav"
+               :key="item.id"
+               :name="item.name" 
+               :section="item.section"
+               />
             </ul>
          </div>
-        <section id="about">about</section>
-        <section id="contacts">contacts</section>
-        <section id="terms">Terms</section>
-         <section id="privacy-policy">privacy-policy</section>
       </div>
+    </div>
+  </div>
+  <div class="section">
+    <div class="container">
+      <section id="about" class="section__wrapper">
+        <p class="heading">
+          About
+        </p>
+      </section>
+    </div>
+  </div>
+    <div class="section">
+    <div class="container">
+      <section id="contacts" class="section__wrapper">
+        <p class="heading">
+          Contacts
+        </p>
+      </section>
+    </div>
+  </div>
+    <div class="section">
+    <div class="container">
+      <section id="terms" class="section__wrapper">
+        <p class="heading">
+          Terms
+        </p>
+      </section>
+    </div>
+  </div>
+    <div class="section">
+    <div class="container">
+      <section id="privacy-policy" class="section__wrapper">
+        <p class="heading">
+         Privacy Policy
+        </p>
+      </section>
     </div>
   </div>
 </template>
 
 <script setup>
+import SmallNavItem from "@/components/core/SmallNavItem.vue"
+
+
+const nav = [
+  {
+    id: '1',
+    name: 'About',
+    section:' #about',
+  },
+    {
+    id: '2',
+    name: 'Contacts',
+    section:'#contacts',
+  },
+    {
+    id: '3',
+    name: 'Terms',
+    section:'#terms',
+  },
+    {
+    id: '4',
+    name: 'Privacy policy',
+    section:'#privacy-policy',
+  }
+]
 </script>
 
  

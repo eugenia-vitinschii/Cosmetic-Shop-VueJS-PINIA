52d18c93043bf2d3ceb4ffbf3bab2a76094463e6
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-01-17 15:40:41 +0200

    created cart.scss, edit Shopping Item

--- src/components/sections/ShoppingItem.vue
+++ src/components/sections/ShoppingItem.vue
@@ -1,79 +1,85 @@
 <template>
-   <div class="cart__item">
-      <div class="cart__img">
+  <div class="cart__item">
+    <div class="cart__img">
       <router-link :to="'/product/' + id">
-          <img
-            :src="image_link"
-            @error="$event.target.src = api_featured_image"
-          />
-        </router-link>
-      </div>
-      <div class="cart__info">
-           <router-link :to="'/product/' + id" class="body-text">
+        <img
+          :src="image_link"
+          @error="$event.target.src = api_featured_image"
+        />
+      </router-link>
+    </div>
+    <div class="cart__info">
+      <div class="cart__info-name">
+        <router-link :to="'/product/' + id" class="body-text">
           {{ name }}
         </router-link>
-                   <router-link :to="'/product-type/' + product_type" class="small-text">
-            {{ product_type }}
-          </router-link>
-           <router-link :to="'/category/' + category" class="small-text" v-if="category">
-            /{{ category }}</router-link
-          >
-         <p class="body-text">{{color}}</p>
       </div>
-      <div class="cart__counter">
-         <p class="body-text">{{product_colors}}</p>
+      <div class="cart__info-description">
+        <router-link :to="'/product-type/' + product_type" class="small-text">
+          {{ product_type }}
+        </router-link>
+        <router-link
+          :to="'/category/' + category"
+          class="small-text"
+          v-if="category"
+        >
+          /{{ category }}</router-link
+        >
+        <p class="body-text">{{ color }}</p>
       </div>
-      <div class="cart__price">
-         <p class="body-text">{{price}} {{ price_sign }} ({{currency}})</p> 
-         </div>
-      <div class="cart__delete">
-         <button>
-            delete
-         </button>
-      </div>
-   </div>
+    </div>
+    <div class="cart__counter">
+      <p class="body-text">{{ product_colors }}</p>
+    </div>
+    <div class="cart__price">
+      <p class="body-text">{{ price }} {{ price_sign }} ({{ currency }})</p>
+    </div>
+    <div class="cart__delete">
+      <button>delete</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {defineOptions, defineProps} from "vue";
+import { defineOptions, defineProps } from "vue";
 
 defineOptions({
-   name: "ShoppingItem"
-})
+  name: "ShoppingItem",
+});
 //props
 defineProps({
-   id: {
-      type: [String, Number],
-   },
-   image_link: {
-      type: String,
-   },
-   api_featured_image: {
-      type: String,
-   },
-   name: {
-      type: String,
-   },
-   color: {
-   type: String,
-   },
-   product_type:{
-       type: String,
-   },
-   category:{
-       type: String,
-   },
-   product_colors:{
-       type: String,
-   },
-   price:{
-      type: Number,
-   },
-   price_sign:{
-       type: String,
-   },
-   currency:{
-       type: String,
-   },
-})
+  id: {
+    type: [String, Number],
+  },
+  image_link: {
+    type: String,
+  },
+  api_featured_image: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  product_type: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  product_colors: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  price_sign: {
+    type: String,
+  },
+  currency: {
+    type: String,
+  },
+});
 </script>

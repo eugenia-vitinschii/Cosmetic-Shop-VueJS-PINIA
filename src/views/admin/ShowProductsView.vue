7000f14dc0d7b67060efca4c58862f87457cfd7a
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-02-27 21:28:52 +0200

    created HTML for show products view (admin), imported cosmetic store from pinia

--- src/views/admin/ShowProductsView.vue
+++ src/views/admin/ShowProductsView.vue
@@ -1,25 +1,67 @@
 <template>
-   <div class="admin">
-      <div class="container">
-         <div class="admin__wrapper">
-            <p class="heading">Show  All products</p>
-            <the-admin-header/>
-         </div>
+  <div class="admin">
+    <div class="container">
+      <div class="admin__wrapper">
+         <the-admin-header /> 
+        <p class="heading">All products {{cosmetics.length}} (produse)</p>
+       
+        <div class="admin-content">
+          <table class="admin__table">
+            <tr>
+              <th>Brand</th>
+              <th>Name</th>
+              <th>Category / Product Type</th>
+              <th>Șterge</th>
+              <th>Redactează</th>
+            </tr>
+            <tr v-for="product in cosmetics" :key="product.id">
+              <td>{{ product.brand }}</td>
+              <td>{{ product.name }}</td>
+              <td>{{ product.category }}/ {{ product.product_type }}</td>
+              <td>
+                <button class="red-button">
+                  Șterge @click="deleteItem(product.id)"
+                </button>
+              </td>
+              <td>
+                <router-link class="green-button" :to="'/update/' + product.id">
+                  Redactează
+                </router-link>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
-   </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
 //vue
-import {defineOptions} from "vue";
+import { defineOptions } from "vue";
+
+// import vue hooks
+import { onMounted } from "vue";
 
 //import components
-import TheAdminHeader from "@/components/layout/TheAdminHeader.vue"
+import TheAdminHeader from "@/components/layout/TheAdminHeader.vue";
 
 //component settings
 defineOptions({
-   name: "ShowProductsView"
-})
+  name: "ShowProductsView",
+});
 
+//pinia store imports
+import { useCosmeticStore } from "@/stores/cosmetic";
+import { storeToRefs } from "pinia";
+
+//pinia variables
+const store = useCosmeticStore();
+const { cosmetics } = storeToRefs(store);
+const { fetchCosmetics } = store;
+
+// use hooks
+onMounted(() => {
+  fetchCosmetics();
+});
 </script>

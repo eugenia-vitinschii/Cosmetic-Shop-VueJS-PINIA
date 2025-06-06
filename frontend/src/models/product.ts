b73b1dab852e5d6893431eb34250b003592008fd
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-06-06 15:29:28 +0300

    feat: create product model and interface

---
+++ frontend/src/models/product.ts
@@ -0,0 +1,77 @@
+//product constructor
+
+//product color (hex + name)
+export interface ProductColor {
+   hex_value: string;
+   colour_name: string;
+}
+
+export interface ProductData {
+   id?: number;
+   quantity?: number;
+   brand?: string;
+   name?: string;
+   price?: number;
+   price_sign?: string;
+   currency?: string;
+   image_link?: string;
+   product_link?: string;
+   website_link?: string;
+   description?: string;
+   rating?: number;
+   category?: string;
+   product_type?: string;
+   tag_list?: string[];
+   created_at?: string;
+   updated_at?: string;
+   product_api_url?: string;
+   api_featured_image?: string;
+   product_colors?: ProductColor[];
+}
+
+export class Product {
+   id?: number;
+   quantity?: number;
+   brand?: string;
+   name?: string;
+   price?: number;
+   price_sign?: string;
+   currency?: string;
+   image_link?: string;
+   product_link?: string;
+   website_link?: string;
+   description?: string;
+   rating?: number;
+   category?: string;
+   product_type?: string;
+   tag_list?: string[];
+   created_at?: string;
+   updated_at?: string;
+   product_api_url?: string;
+   api_featured_image?: string;
+   product_colors?: ProductColor[];
+
+
+   constructor(data: ProductData = {}) {
+      this.id = data.id;
+      this.quantity = data.quantity;
+      this.brand = data.brand;
+      this.name = data.name;
+      this.price = data.price;
+      this.price_sign = data.price_sign;
+      this.currency = data.currency;
+      this.image_link =  data.image_link;
+      this.product_link = data.product_link;
+      this.website_link = data.website_link;
+      this.description = data.description ;
+      this.rating = data.rating;
+      this.category = data.category;
+      this.product_type = data.product_type;
+      this.tag_list = data.tag_list;
+      this.created_at = data.created_at;
+      this.updated_at = data.updated_at ;
+      this.product_api_url = data.product_api_url ;
+      this.api_featured_image = data.api_featured_image;
+      this.product_colors = data.product_colors ;
+   }
+}

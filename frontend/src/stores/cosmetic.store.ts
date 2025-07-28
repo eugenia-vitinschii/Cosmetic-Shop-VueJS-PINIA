75ab27d5412909e97dad95f2916bd2c0d983f84a
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-07-28 18:59:29 +0300

    refactor(stores/cosmetic): use composable store structure, implement API and types

--- frontend/src/stores/cosmetic.store.ts
+++ frontend/src/stores/cosmetic.store.ts
@@ -1,171 +1,76 @@
 //cosmetic store
 
-//pinia
-import { defineStore  } from "pinia";
-//axios
-import axios from "axios";
-import { Product  } from "@/models/product"
 
-//base url
-const baseUrl = "http://localhost:3000";
+import { defineStore } from "pinia";
+import { computed, reactive } from "vue";
 
-//interface state
-interface UserData{
-  cart: Product[],
-  favorite: Product[]
-}
-interface CosmeticState {
-  cosmetics: Product []
-  limit: number,
-  page: number,
-  complected: boolean,
-  count: 1,
-  user: UserData,
-}
+import type { CosmeticState } from "@/types/cosmetic"
 
-export const useCosmeticStore = defineStore( "cosmeticId", {
-   state: (): CosmeticState => ({
-      cosmetics: [],
-      limit: 30,
-      page: 1,
-      count: 1,
-      complected: true,
-      user:{
-        cart: [],
-        favorite: [],
-      }
-   }),
-   getters: {
-    //products by brand
-    fileredByBrand: (state) => {
-      return (brandName: string) => state.cosmetics.filter((item) => item.brand === brandName )
-    },
-    //products by productType
-    fileredByProductType: (state) => {
-      return (productType: string) => state.cosmetics.filter((item) =>  item.product_type ===  productType )
-    },
-    // category
-    fileredByCategory: (state) => {
-      return (theCategory:string) => state.cosmetics.filter((item) => item.category === theCategory)
-    },
-    //brands
-    allBrand: (state) => {
-      return [...new Set(state.cosmetics.map((item) => item.brand))]
-    },
-    //product types
-    allProductTypes: (state) => {
-      return [...new Set(state.cosmetics.map((item) => item.product_type))]
-    },
-    // category
-    allCategory: (state) => {
-      return [...new Set(state.cosmetics.map((item) => item.category))]
+import { api } from "@/utils/api"
+
+export const useCosmeticStore = defineStore("cosmetic", () => {
+  const cosmetics = reactive<CosmeticState>({
+    products: [],
+    limit: 30,
+    page: 1,
+    complected: true,
+    count: 1
+  })
+
+  const fileredByBrand = (brandName: string) => {
+    return cosmetics.products.filter((item) => item.brand === brandName)
+  };
+
+  const fileredByProductType = (productType: string) => {
+    return cosmetics.products.filter((item) => item.product_type === productType)
+  };
+
+  const fileredByCategory = (theCategory: string) => {
+    return cosmetics.products.filter((item) => item.category === theCategory)
+  };
+
+  const allBrands = computed(() => {
+    return [...new Set(cosmetics.products.map((item) => item.brand))]
+  });
+
+  const allProductTypes = () => {
+    return [...new Set(cosmetics.products.map((item) => item.product_type))]
+  };
+
+  const allCategory = computed(() => {
+    return [...new Set(cosmetics.products.map((item) => item.category))]
+  })
+
+  async function fetchCosmetics() {
+    try {
+      const response = await api.get("/cosmetics");
+      cosmetics.products = response.data;
+    } catch (error) {
+      console.log("fetchCosmetics error:", error);
     }
-   },
-   actions: {
-    // fetch cosmetics from db.json
-      async fetchCosmetics() {
-         try {
-           const response = await axios.get(`${baseUrl}/cosmetics`);
-           this.cosmetics = response.data;
-         } catch (error) {
-           console.log("fetchCosmetics error:", error);
-         }
-       },
-    //create 
-       async createProduct(payload: Product) {
-        if (!payload.name || !payload.brand) return; 
-        try{
-          const createdAt = new Date().toISOString()
-          const {data} = await axios.post(`${baseUrl}/cosmetics`,{
-            ...payload,
-            created_at: createdAt,
-            updated_at: createdAt,
-          });
-          this.cosmetics.push(data);
-        }catch (err){
-          console.log(err)
-        }
-       },
-       //updateItem
-       async updateProduct(id: string, payload: Product){
-        try{
-          const {data} = await axios.put(`${baseUrl}/cosmetics/${id}`, payload);
-            
-          const index = this.cosmetics.findIndex(p => p.id === id);
-          if( index !== -1){
-            this.cosmetics[index] = data;
-          }
-        } catch(err){
-          console.log(err)
-        } 
-       },
-       //fetch by id
-       async fetchProductById(id: string) {
-         // get Cosmetics from db.json by id
-         try {
-           const response = await axios.get(`${baseUrl}/cosmetics/${id}`);
-           this.cosmetics = [response.data];
-         } catch (error) {
-           console.log("fetchProductById(id) error:", error);
-         }
-       },
-        // delete product from db.json
-        async deleteProduct(id: string) {
-          try {
-            await axios.delete(`${baseUrl}/cosmetics/${id}`);
-            this.cosmetics = this.cosmetics.filter((item) => item.id !== id);
-          } catch (err) {
-            console.error("Delete item ERROR!", err);
-          }
-        },
-      //load more
-       async loadMore(){
-        this.page++
-        try {
-          const response = await axios.get(`${baseUrl}/cosmetics?_page=${this.page}&_limit=${this.limit}`);
-          this.cosmetics.push(...response.data);
-          if(response.data.length < this.limit){
-            this.complected = false
-          }
-        }catch (error){
-          console.log('load more err:', error);
-        }
-       },
-       //add to wish list
-       addToCart(item: Product) {
-        const index = this.user.cart.findIndex((p) => p.id == item.id);
-        if ( index !== -1){
-          this.user.cart[index].quantity = ( this.user.cart[index].quantity || 0) + 1
-        } else {
-          item.quantity = 1;
-          this.user.cart.push(item)
-        }
-        localStorage.setItem("cart",JSON.stringify(this.user.cart));
-       },
-       //add to favorite
-       addToFavorite(item: Product){
-        const index = this.user.favorite.findIndex((p) => p.id == item.id);
-        if ( index !== -1){
-          this.user.favorite[index].quantity = (this.user.favorite[index].quantity || 0) +1;
-        } else {
-          item.quantity = 1;
-          this.user.favorite.push(item)
-        }
-        localStorage.setItem("favorite",JSON.stringify(this.user.favorite));
-      //incriment
-       },
-       incrementQuantity(id: string) {
-        const item = this.user.cart.find((p) => p.id === id);
-        if(item) {
-          item.quantity = (item.quantity || 0) +1
-        }
-      },
-      removeItem(id:string){
-        this.user.cart = this.user.cart.filter((item) => item.id !== id)
-      },
-      removeFromFavorites(id: string){
-        this.user.favorite = this.user.favorite.filter((item) => item.id !== id)
+  }
+
+  async function fetchProductById(id: string) {
+    try {
+      const response = await api.get(`/cosmetics/${id}`);
+      cosmetics.products = [response.data];
+    } catch (error) {
+      console.log("fetchProductById(id) error:", error);
+    }
+  }
+
+  async function loadMore() {
+    cosmetics.page++
+    try {
+      const response = await api.get(`/cosmetics?_page=${cosmetics.page}&_limit=${cosmetics.limit}`);
+      cosmetics.products.push(...response.data);
+      if (response.data.length < cosmetics.limit) {
+        cosmetics.complected = false
       }
-   }
+    } catch (error) {
+      console.log('load more err:', error);
+    }
+  }
+  return { fileredByBrand, fileredByProductType, fileredByCategory, allBrands, allProductTypes, allCategory, fetchCosmetics, fetchProductById, loadMore }
 })
 

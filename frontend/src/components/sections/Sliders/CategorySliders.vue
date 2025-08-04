6fcb915ec9332832ac0bb215fbe5046975983dcf
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-08-04 16:30:22 +0300

    refactor(CategorySliders): migrate to <script setup lang=ts>, integrate ProductCard, use user store

--- frontend/src/components/sections/Sliders/CategorySliders.vue
+++ frontend/src/components/sections/Sliders/CategorySliders.vue
@@ -1,141 +1,73 @@
 <template>
   <swiper
-    :pagination="{
-      type: 'fraction',
-    }"
     :modules="modules"
-    :slidesPerView="4"
-    :aautoplay="{
+    :autoplay="{
       delay: 2500,
       disableOnInteraction: false,
     }"
-                    :breakpoints= "{
-            1054:{
-             slidesPerView: 5,
-            },
-                896:{
-                slidesPerView: 4,
-
-              },
-              663:{
-                slidesPerView: 3,
-              }
-              ,
-             200:{
-                slidesPerView: 2,
-              }
-            }"
+    :breakpoints= "{
+      1054:{ slidesPerView: 5},
+      896:{slidesPerView: 4,},
+      663:{slidesPerView: 3,},
+      200:{slidesPerView: 2,}
+    }"
     class="mySwiper"
   >
-    <swiper-slide >
-      <!-- <the-item
-       v-for="item in category" :key="item.id"
-        :key="item.id"
-        :id="item.id"
-        :image_link="item.image_link"
-        :api_featured_image="item.api_featured_image"
-        :name="item.name"
-        :product_type="item.product_type"
-        :price="item.price"
-        :price_sign="item.price_sign"
-        :category="item.category"
-        :brand="item.brand"
-        :currency="item.currency"
-        :rating="item.rating"
-        @addItemCart="addToCart(item)"
-        @addToFavorite="addToFavorite(item)"
-      /> -->
+    <swiper-slide v-for="item in cosmetic.productsByTag('category')" :key="item.id">
+      <the-product-card
+          :id="item.id"
+          :image_link="item.image_link"
+          :api_featured_image="item.api_featured_image"
+          :name="item.name"
+          :product_type="item.product_type"
+          :price="item.price"
+          :price_sign="item.price_sign"
+          :category="item.category"
+          :brand="item.brand"
+          :rating="item.rating"
+          :currency="item.currency"
+          :isFavorite="item.id ? user.isFavorite(item.id) :false"
+          @toggleFavorite="user.toggleFavorite(item)"
+      />
     </swiper-slide>
   </swiper>
 </template>
 
- 
+<script setup lang="ts">
+//vue
+import { defineOptions, onMounted } from "vue";
 
-  <script>
+//pinia
+import { useCosmeticStore } from "@/stores/cosmetic.store";
+import { useUserStore } from "@/stores/user.store";
+
+// components
+import TheProductCard from "@/components/TheProductCard.vue";
+
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from "swiper/vue";
 
 // Import Swiper styles
 import "swiper/css";
-
-import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import required modules
-import { Autoplay, Pagination } from "swiper/modules";
-
-export default {
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
-  setup() {
-    return {
-      modules: [Autoplay, Pagination],
-    };
-  },
-};
-</script>
+import { Autoplay } from "swiper/modules";
 
 
-<script setup>
-//vue
-import { defineOptions, onMounted } from "vue";
+//pinia variables
+const cosmetic = useCosmeticStore();
+const user = useUserStore();
 
-//components
-import TheItem from "@/components/TheItem.vue";
+const modules = [Autoplay];
 
-//pinia
-// import { useSliderStore } from "@/stores";
-
-// import { storeToRefs } from "pinia";
 
 defineOptions({
   name: "CategorySliders",
 });
 
-// // store
-// const store = useSliderStore();
-// const { category } = storeToRefs(store);
-// const { fetchCategoryProducts, pushToCart, pushToFavorite } = store;
 
-// // add to wish list
-// function addToCart(item) {
-//   pushToCart({
-//     id: item.id,
-//     image_link: item.image_link,
-//     api_featured_image: item.api_featured_image,
-//     color: item.color,
-//     name: item.name,
-//     product_type: item.product_type,
-//     price: item.price,
-//     price_sign: item.price_sign,
-//     category: item.category,
-//     brand: item.brand,
-//     currency: item.currency,
-//     active: item.active,
-//   });
-// }
-// // add to favorite
-// function addToFavorite(category) {
-//   pushToFavorite({
-//     id: category.id,
-//     image_link: category.image_link,
-//     api_featured_image: category.api_featured_image,
-//     color: category.color,
-//     name: category.name,
-//     product_type: category.product_type,
-//     price: category.price,
-//     price_sign: category.price_sign,
-//     category: category.category,
-//     brand: category.brand,
-//     currency: category.currency,
-//     product_colors: category.product_colors,
-//     quantity: 1,
-//     active: true,
-//   });
-// }
-// onMounted(() => {
-//   fetchCategoryProducts();
-// });
+onMounted(() => {
+cosmetic.fetchCosmetics();
+});
 </script>

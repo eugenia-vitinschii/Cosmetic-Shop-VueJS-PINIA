e05f11848eb3366d7b33b026a4784cd2a32f2f09
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-08-15 19:20:36 +0300

    remove(user.store): delete deprecated functions - addToFavorite, incrementAuantity, removeFromCart, removeFromFavorites

--- frontend/src/stores/user.store.ts
+++ frontend/src/stores/user.store.ts
@@ -56,28 +56,5 @@ export const useUserStore = defineStore("user", () => {
     }
     localStorage.setItem("cart", JSON.stringify(user.cart));
   }
-  //delete
-  function addToFavorite(item: Product) {
-    const index = user.favorite.findIndex((p) => p.id == item.id);
-    if (index !== -1) {
-      user.favorite[index].quantity = (user.favorite[index].quantity || 0) + 1;
-    } else {
-      item.quantity = 1;
-      user.favorite.push(item)
-    }
-    localStorage.setItem("favorite", JSON.stringify(user.favorite));
-  }
-  function incrementQuantity(id: string) {
-    const item = user.cart.find((p) => p.id === id);
-    if (item) {
-      item.quantity = (item.quantity || 0) + 1
-    }
-  }
-  function removeFromCart(id: string) {
-    user.cart = user.cart.filter((item) => item.id !== id)
-  }
-  function removeFromFavorites(id: string) {
-    user.favorite = user.favorite.filter((item) => item.id !== id)
-  }
-  return { user, favorite ,  addToCart, addToFavorite, incrementQuantity, removeFromCart, removeFromFavorites, loadFavorite , toggleFavorite, isFavorite, resetFavorite}
+  return { user, favorite ,  addToCart, loadFavorite , toggleFavorite, isFavorite, resetFavorite}
 })

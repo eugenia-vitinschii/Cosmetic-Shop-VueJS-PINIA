fc9b4021488199ff3d39d240e7fa42c79f2b5e5a
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-09-05 18:28:18 +0300

    feat(Breadcrumbs): built page nav component

---
+++ frontend/src/components/core/Breadcrumbs.vue
@@ -0,0 +1,33 @@
+<template>
+<nav 
+   class="breadcrumbs"
+   aria-label="Breadcrumb"
+>
+   <router-link
+      v-for="(item, index) in items"
+      :key="index"
+      :to="item.to"
+      class="breadcrumbs__link body-text link " 
+   >
+      {{ item.label }} /
+   </router-link>
+   <!-- current page -->
+   <span class="breadcrumbs__current body-text" > {{ current }}</span>
+</nav>
+</template>
+
+<script setup lang="ts">
+
+defineOptions({
+   name: "TheBreadcrumbs"
+})
+
+//props
+const props = defineProps<{
+   items: {
+      label: string;
+      to: string
+   }[];
+   current: string;
+}>()
+</script>

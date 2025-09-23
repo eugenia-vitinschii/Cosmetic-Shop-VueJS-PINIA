9237b33f0d0dc8cfa2c6ff74bbcb495feb44c305
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-09-23 19:47:19 +0300

    feat(EmptyState): create component for empty state - favorite, cart

---
+++ frontend/src/components/ui/EmptyState.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="empty">
+    <div class="empty__text">
+      <p class="subheading">
+        Your {{ entity }} is empty.
+        <router-link 
+          :to="link"
+          class="subheading">
+          Pick some Products!
+        </router-link>
+      </p>
+    </div>
+    <div class="empty__img">
+      <img 
+        :src="image || defaultImage" 
+        alt="Empty state"
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+//Component settings
+defineOptions({
+  name: 'MyComponent'
+})
+
+defineProps<{
+  entity: string
+  link: string
+  image?: string
+}>()
+
+const defaultImage = new URL('@/assets/img/empty-cart.jpg', import.meta.url).href
+
+</script>

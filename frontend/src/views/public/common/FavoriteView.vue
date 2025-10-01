b953e44ac80e085b6e551f921f2eea7998148292
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-01 15:56:53 +0300

    refactor(FavoriteView): intergated new components,polished sass rules

--- frontend/src/views/public/common/FavoriteView.vue
+++ frontend/src/views/public/common/FavoriteView.vue
@@ -3,14 +3,32 @@
 <div class="favorite"> 
    <div class="container">
       <div class="favorite__wrapper">
-         <div class="favorite__title">
-            <p class="heading" v-if="favorite.length >= 1">Favorite ({{favorite.length}})</p>
-            <router-link to="/" class="body text" v-else>No Favorite items, add now!</router-link>
+        <!-- page header -->
+        <div class="page-header">
+            <back-button/>
+            <the-breadcrumbs
+              :items="[
+                {label: 'home', to: '/'},
+              ]"
+              :current="'favorite'"
+            />
          </div>
-         
+         <!--- entity header -->
+         <div class="entity-header" v-if="favorite.length > 0">
+            <p class="heading">Favorite ({{favorite.length}})</p>
+            <button @click="user.resetFavorite()"> 
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"/></svg>
+            </button>
+         </div>
+          <empty-state
+            v-else
+            :entity="'favorite'"
+            :link="'/'"
+          />
+
          <!-- favorite items -->
          <div class="favorite__items products-wrapper" v-if="created">
-            <button @click="user.resetFavorite()"> clear</button>
+            
           <product-card
          v-for="item in favorite"
           :key="item.id"
@@ -54,6 +72,11 @@
 <script setup lang="ts">
 //vue
 import {ref,onMounted } from "vue"
+
+//components
+import TheBreadcrumbs from "@/components/core/Breadcrumbs.vue"
+import BackButton from "@/components/core/BackButton.vue";
+import EmptyState from "@/components/ui/EmptyState.vue";
 import ProductCard from "@/components/product/ProductCard.vue";
 import TopSliders from "@/components/sliders/TopSliders.vue";
 import CategorySliders from "@/components/sliders/CategorySliders.vue";

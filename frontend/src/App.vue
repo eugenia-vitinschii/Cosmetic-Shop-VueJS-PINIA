0b8c6599229f7ca62a21f9f2b45d5e39c987152f
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-10 16:58:59 +0300

    feat(app.vue): create custom theme color for /admin routes, remove header, footer from /admin routes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,28 @@
 <template>
-<the-header />
+  <div :class="['app-wrapper', {'admin-theme': isAdminRoute}]">
+    <the-header v-if="!isAdminRoute" />
   <!-- content -->
   <div class="content">
     <router-view />
   </div>
   <!-- footer -->
-<the-footer/>
+<the-footer v-if="!isAdminRoute"/>
+  </div>
+
 </template>
 
 <script setup lang="ts">
+//vue
+import {useRoute} from 'vue-router'
+import {computed} from 'vue'
+
+//components
 import TheHeader from '@/components/layout/TheHeader.vue';
 import TheFooter from '@/components/layout/TheFooter.vue';
+
+//variables
+const route = useRoute()
+
+const isAdminRoute = computed(() => route.path.startsWith('/admin'))
+
 </script>

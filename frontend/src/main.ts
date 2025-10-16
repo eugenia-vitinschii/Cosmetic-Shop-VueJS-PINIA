32013fdf3b3e57f23e81e21278075b783cb9888b
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-16 17:19:45 +0300

    feat(main.ts): initializa cosmetic.store globally on app mount

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,14 @@ import { createApp } from 'vue'
 import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { useCosmeticStore } from './stores/cosmetic.store'
 
 import './assets/sass/app.sass'
 
+
 createApp(App).use(createPinia()).use(router).mount('#app')
+
+
+
+const cosmetic = useCosmeticStore()
+await cosmetic.fetchCosmetics()

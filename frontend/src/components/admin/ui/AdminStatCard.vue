faa11b2eb4cd734b233385b1de95da8f0313cddc
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-16 17:25:32 +0300

    feat(AdminDashboard): integrate AdminStatCard component and new admin-store getters

---
+++ frontend/src/components/admin/ui/AdminStatCard.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="admin-stat-card">
+      <div class="admin-stat-card__icon">
+         <slot name="icon"></slot>
+      </div>
+      <div class="admin-stat-card">
+         <p class="admin-body-text">{{ label }}</p>
+         <p class="admin-subheading">{{ value }}</p>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+//vue 
+
+
+//Component settings
+defineOptions({
+  name: 'AdminStatsCards'
+})
+
+
+defineProps<{
+   label: string
+   value: string |number
+}>()
+</script>

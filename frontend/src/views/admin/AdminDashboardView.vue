61512f3881047e1b5e19bd3c692adf8afc814969
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-20 18:27:41 +0300

    style(AdminChart): introduced sass rules for .admin-analitics, admin-chart using grid

--- frontend/src/views/admin/AdminDashboardView.vue
+++ frontend/src/views/admin/AdminDashboardView.vue
@@ -5,8 +5,8 @@
             <the-admin-header/>
             <section class="admin-stats">
                <admin-stat-card
-               label="Total Products"
-               :value="admin.totalProducts"
+                  :value="admin.totalProducts"
+                  label="Total Products"
                >
                   <template #icon>
                      <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
@@ -16,8 +16,8 @@
                   label="Total Brands"
                   :value="admin.totalBrands"
                >
-               <template #icon>
-               <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"  ><path d="M342-160h276l40-160H302l40 160Zm0 80q-28 0-49-17t-28-44l-45-179h520l-45 179q-7 27-28 44t-49 17H342ZM200-400h560v-80H200v80Zm280-240q0-100 70-170t170-70q0 90-57 156t-143 80v84h320v160q0 33-23.5 56.5T760-320H200q-33 0-56.5-23.5T120-400v-160h320v-84q-86-14-143-80t-57-156q100 0 170 70t70 170Z"/></svg>               </template>
+                  <template #icon>
+                     <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"  ><path d="M342-160h276l40-160H302l40 160Zm0 80q-28 0-49-17t-28-44l-45-179h520l-45 179q-7 27-28 44t-49 17H342ZM200-400h560v-80H200v80Zm280-240q0-100 70-170t170-70q0 90-57 156t-143 80v84h320v160q0 33-23.5 56.5T760-320H200q-33 0-56.5-23.5T120-400v-160h320v-84q-86-14-143-80t-57-156q100 0 170 70t70 170Z"/></svg>               </template>
                </admin-stat-card>
                <admin-stat-card
                   label="Total Categories"
@@ -33,6 +33,26 @@
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" ><path d="M280-80v-160H0l154-240H80l280-400 120 172 120-172 280 400h-74l154 240H680v160H520v-160h-80v160H280Zm389-240h145L659-560h67L600-740l-71 101 111 159h-74l103 160Zm-523 0h428L419-560h67L360-740 234-560h67L146-320Zm0 0h155-67 252-67 155-428Zm523 0H566h74-111 197-67 155-145Zm-149 80h160-160Zm201 0Z"/></svg>                  </template>
                </admin-stat-card>
             </section>
+            <section class="admin-analytics">
+               <admin-chart
+                  title="Products by Category"
+                  type="pie"
+                  :labels="admin.categoryLabels"
+                  :data="admin.categoryData"
+               />
+               <admin-chart
+                  title="Products by Brand"
+                  type="bar"
+                  :labels="admin.brandLabels"
+                  :data="admin.brandData"
+               />
+               <admin-chart
+                  title="Products by Types"
+                  type="line"
+                  :labels="admin.typesLabels"
+                  :data="admin.typesData"
+               />
+            </section>
          </div>
       </div>
    </div>
@@ -43,6 +63,7 @@
 //import components
 import TheAdminHeader from "@/components/layout/TheAdminHeader.vue"
 import AdminStatCard from "@/components/admin/ui/AdminStatCard.vue";
+import AdminChart from "@/components/admin/ui/AdminChart.vue";
 
 //pinia store imports
 import { useAdminStore } from "@/stores/admin.store"
@@ -51,7 +72,6 @@ import { useAdminStore } from "@/stores/admin.store"
 defineOptions({
    name: "AdminDashboardView"
 })
-
 
 const admin = useAdminStore()
 </script>

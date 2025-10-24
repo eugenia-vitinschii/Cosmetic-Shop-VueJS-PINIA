e85d1e25857514d0dd4359901b0cdd09a88c46e4
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-24 17:16:56 +0300

    feat(AdminDashboard):add new div for to do inside of first sections, edit sass rules for section

--- frontend/src/views/admin/AdminDashboardView.vue
+++ frontend/src/views/admin/AdminDashboardView.vue
@@ -3,8 +3,12 @@
       <div class="container">
          <div class="admin__wrapper">
             <the-admin-header/>
-            <section class="admin-stats">
-               <admin-stat-card
+            <section class="admin-top">
+               <div class="admin-todos">
+                  <admin-todo/>
+               </div>
+               <div class="admin-stats">
+                  <admin-stat-card
                   :value="admin.totalProducts"
                   label="Total Products"
                >
@@ -32,6 +36,8 @@
                   <template #icon>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" ><path d="M280-80v-160H0l154-240H80l280-400 120 172 120-172 280 400h-74l154 240H680v160H520v-160h-80v160H280Zm389-240h145L659-560h67L600-740l-71 101 111 159h-74l103 160Zm-523 0h428L419-560h67L360-740 234-560h67L146-320Zm0 0h155-67 252-67 155-428Zm523 0H566h74-111 197-67 155-145Zm-149 80h160-160Zm201 0Z"/></svg>                  </template>
                </admin-stat-card>
+               </div>
+
             </section>
             <section class="admin-analytics">
                <admin-chart
@@ -65,6 +71,7 @@
 
 //import components
 import TheAdminHeader from "@/components/layout/TheAdminHeader.vue"
+import AdminTodo from "@/components/admin/ui/AdminTodo.vue";
 import AdminStatCard from "@/components/admin/ui/AdminStatCard.vue";
 import AdminChart from "@/components/admin/ui/AdminChart.vue";
 import AdminRecentProducts from "@/components/admin/ui/AdminRecentProducts.vue";

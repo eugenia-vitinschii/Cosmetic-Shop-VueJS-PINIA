faa2adf40c4e638908b556fec5e7e845096c1c51
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-27 17:40:26 +0200

    feat(AdminTodo): add svg for buttons, create new classes

--- frontend/src/components/admin/ui/AdminTodo.vue
+++ frontend/src/components/admin/ui/AdminTodo.vue
@@ -5,15 +5,17 @@
       <div class="admin__todo__meta">
         <p class="admin-body-text">{{ todo.stats.done }} / {{ todo.stats.total }}</p>
         <button
+          class="admin-svg-button"
           @click="todo.clearCompleted"
-          v-show="todo.stats.done"
+          v-if="todo.stats.done"
         >
-          delete
-        </button>
+        <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"><path d="M873-88 609-352 495-238 269-464l56-58 170 170 56-56-414-414 56-58 736 736-56 56ZM269-238 43-464l56-56 170 170 56 56-56 56Zm452-226-56-56 196-196 58 54-198 198ZM607-578l-56-56 86-86 56 56-86 86Z"/></svg>
+      </button>
       </div>
     </div>
     <div class="admin-todo__add">
       <input 
+        class="admin-custom-input-text"
         type="text" 
         placeholder="Add new task..."
         v-model="newTask"
@@ -21,9 +23,10 @@
       >
       <button
         @click="onAdd"
+        class="admin-svg-button"
       >
-        add
-      </button>
+      <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"><path d="M360-240 120-480l240-240 56 56-144 144h488v-160h80v240H272l144 144-56 56Z"/></svg>
+    </button>
     </div>
     <ul class="admin-todo__list">
       <li 
@@ -31,13 +34,13 @@
         v-for="i in todo.tasks"
         :key="i.id"
       >
-        <label class="admin-custom-chekbox">
+        <label class="admin-custom-checkbox">
           <input 
             type="checkbox" 
             :checked="i.done"
             @change="todo.toggleTask(i.id)"
           >
-          <span></span>
+          <span class="checkmark"></span>
           <span class="admin-body-text"
             :class="{done: i.done}"
           >
@@ -46,9 +49,10 @@
         </label>
         <button
           @click="todo.deleteTask(i.id)"
+          class="admin-svg-button"
         >
-          x
-        </button>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m456-320 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 160q-19 0-36-8.5T296-192L80-480l216-288q11-15 28-23.5t36-8.5h440q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H360ZM180-480l180 240h440v-480H360L180-480Zm400 0Z"/></svg>
+      </button>
       </li>
     </ul>
   </div>

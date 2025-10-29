44e1bda753320492cd69b48ad9d8aabd12da2588
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-29 17:05:18 +0200

    feat(AdminHeader): implement admin sidebar component, isSidebarOpen for toogle visibility

--- frontend/src/components/layout/TheAdminHeader.vue
+++ frontend/src/components/layout/TheAdminHeader.vue
@@ -1,23 +1,34 @@
 <template>
    <div class="admin-header">
-         <div class="admin-header__wrapper">
-            <ul>
-               <li><router-link to="/admin" class="admin-header__link">Admin Dashboard</router-link></li>
-               <li><router-link to="/admin/all" class="admin-header__link" >Show product list</router-link></li>
-               <li><router-link to="/admin/add" class="admin-header__link">Add new product</router-link></li>
-            </ul>
-            <admin-theme-toggle/>
-
-         </div>
+      <div class="admin-header__wrapper">
+         <button class="admin-svg-button" @click="isSidebarOpen = !isSidebarOpen">
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm280-80h280v-560H480v560Z"/></svg>
+         </button>
+         <p class="admin-body-text">Hi, admin!</p>
+         <admin-theme-toggle/>
+      </div>
+      <admin-sidebar 
+         :open="isSidebarOpen" 
+         @close="isSidebarOpen = false"
+      />
       </div>
 </template>
 
 
 <script setup lang="ts">
+//vue
+import {ref} from 'vue'
+
+//components
 import AdminThemeToggle from '../admin/ui/AdminThemeToggle.vue';
+import AdminSidebar from './AdminSidebar.vue';
 
 //component
 defineOptions({
    name: "AdminHeader"
 })
+
+//variables
+const isSidebarOpen = ref(false)
+
 </script>

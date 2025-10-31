58070ed124769096157324eed85e5e17a6b33556
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-31 16:58:11 +0200

    refactor(ProductDashboard): introduced admin classes, delete basic classes, add id to table

--- frontend/src/views/admin/ProductDashboardView.vue
+++ frontend/src/views/admin/ProductDashboardView.vue
@@ -15,6 +15,7 @@
             <thead>
               <tr>
                 <th  class="admin-body-text">Img </th>
+                <th class="admin-body-text">Id</th>
                 <th class="admin-body-text">Brand</th>
                 <th class="admin-body-text">Name</th>
                 <th class="admin-body-text">Category / Product Type</th>
@@ -31,39 +32,35 @@
                       fallback="@/assets/img/no-img.jpg" alt="Img" />
                   </div>
                 </td>
-                <td class="admin-body-text">{{ product.brand }}</td>
+                <td class="admin-body-text">
+                  {{ product.id }}
+                </td>
+                <td>{{ product.brand }}</td>
                 <td  class="admin-body-text">{{ product.name }}</td>
-                <td class="admin-body-text">{{ product.category }}/ {{ product.product_type }}</td>
+                <td class="admin-body-text">{{ product.category }} <span v-if="product.category && product.product_type">/ </span>{{ product.product_type }}</td>
                 <td>
-                  <div class="input__wrapper">
                     <select 
-                    multiple 
+                    class="admin-select"
                     v-model="product.sliderTags"
                     @change="onSliderChange(product)"
                     >
+                      <option  value="" >clear</option>
                       <option v-for="option in sliderOptions" :key="option" :value="option">
                         {{ option }}
                       </option>
                     </select>
-                    <!-- <span v-if="status.isSaving" >Saving...!!!!</span>
-                    <span v-else-if="status.isSaved">Saved!</span> -->
-                  </div>
                 </td>
                 <td>
-                  <button @click="deleteItem(product.id)" class="delete tooltip">
-                    <span class="tooltip-text small-text">delete</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                      fill="#e8eaed">
+                  <button @click="deleteItem(product.id)" class="admin-svg-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                       <path
                         d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" />
                     </svg>
                   </button>
                 </td>
                 <td>
-                  <router-link :to="'/admin/update/' + product.id" class="edit  tooltip">
-                    <span class="tooltip-text small-text">edit</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                      fill="#e8eaed">
+                  <router-link :to="'/admin/update/' + product.id" class="admin-svg-button">
+                    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" >
                       <path
                         d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
                     </svg>

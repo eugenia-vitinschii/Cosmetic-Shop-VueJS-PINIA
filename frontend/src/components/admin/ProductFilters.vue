113d913fc0f96b0be03f719b4844083166a89830
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-10-31 16:54:40 +0200

    refactor(ProductFilters):apply new classes, change HTML structure

--- frontend/src/components/admin/ProductFilters.vue
+++ frontend/src/components/admin/ProductFilters.vue
@@ -1,40 +1,41 @@
 <template>
 <div class="admin-filters">
-   <!-- brans -->
-   <select v-model="model.brand">
-      <option value="">All brands</option>
-      <option  v-for="i in brands" :key="i" :value="i">{{ i }}</option>
-   </select>
+   <div class="admin-filters__select">
+      <!-- brands -->
+      <select v-model="model.brand" class="admin-select">
+         <option value="">Filter by brands</option>
+         <option  v-for="i in brands" :key="i" :value="i">{{ i }}</option>
+      </select>
    <!-- category -->
-   <select v-model="model.category">
-      <option value="">All categories</option>
-      <option  v-for="i in categories" :key="i" :value="i">{{ i }}</option>
-   </select>
-   <!-- types -->
-   <select v-model="model.type">
-      <option value="">All categories</option>
-      <option  v-for="i in productTypes" :key="i"  :value="i">{{ i }}</option>
-   </select>
+      <select v-model="model.category" class="admin-select">
+         <option value="">Filter by categories</option>
+         <option  v-for="i in categories" :key="i" :value="i">{{ i }}</option>
+      </select>
+      <!-- types -->
+      <select v-model="model.type" class="admin-select">
+         <option value="">Filter by categories</option>
+         <option  v-for="i in productTypes" :key="i"  :value="i">{{ i }}</option>
+      </select>
+   </div>
+   <div class="admin-filters__search">
    <!-- search by name or id -->
     <input type="text"
-    placeholder="Serach by name or ID"
-    v-model="searchLocal"
-    @input="emitDebounched()"
+      class="admin-custom-input-text"
+      placeholder="Serach by name or ID"
+      v-model="searchLocal"
+      @input="emitDebounched()"
     >
     <!-- reset -->
    <button
-   class="delete"
-   type="button"
-    @click="reset"
+      class="admin-svg-button"
+      type="button"
+      @click="reset"
    >
-   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m456-320 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 160q-19 0-36-8.5T296-192L80-480l216-288q11-15 28-23.5t36-8.5h440q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H360ZM180-480l180 240h440v-480H360L180-480Zm400 0Z"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path d="M120-280v-80h560v80H120Zm80-160v-80h560v80H200Zm80-160v-80h560v80H280Z"/></svg>
    </button>
+   </div>
 </div>
-
 </template>
-
-
-
 
 <script setup lang="ts">
 import { ref, watch} from "vue";
@@ -45,7 +46,6 @@ type Filters = {
    type: string;
    query: string;
 }
-
 
 const props = defineProps<{
    brands: string[];
@@ -60,14 +60,12 @@ const emit = defineEmits<{'update:modelValue': [Filters]}>();
 //use local copy for control debounche
 const model = ref<Filters>({...props.modelValue});
 
-
 watch(() => props.modelValue, v => (model.value = { ...v}))
 
 //search
 const searchLocal = ref(model.value.query);
 
 let timer: ReturnType<typeof setTimeout> | null = null;
-
 
 function emitDebounched() {
     if(timer) clearTimeout(timer);

2a1a31c13965895d428a1f48f8235a36948294b9
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-11-08 14:34:11 +0200

    reafactor(ProductColorTabd): add div for existed color, and div for create new color use input color and input text for add hex value and span for show colors, input text for add title

--- frontend/src/components/admin/tabs/ProductColorsTab.vue
+++ frontend/src/components/admin/tabs/ProductColorsTab.vue
@@ -1,104 +1,104 @@
 <template>
   <div class="admin-form-item">
-    <p class="admin-subheading">Colors</p>
-    <div
+    <h3 class="admin-subheading">Colors</h3>
+    <div  
+      class="admin-form-item__color "
       v-for="(color, index) in  modelValue.product_colors"
       :key="index"
-      class="color-row"
     >
       <input
+        class="admin-custom-input-color"
         type="color"
         v-model="color.hex_value"
-         @input="addColorIfNeeded"
-        class="color-picker"
       />
-
       <input
+      class="admin-custom-input-text"
         type="text"
         placeholder="Hex value"
         v-model="color.hex_value"
-         @input="addColorIfNeeded"
       />
-<span class="circle" :style="{ backgroundColor: color.hex_value }"></span>
+      <span class="admin-form-item__hex" :style="{ backgroundColor: color.hex_value }"></span>
       <input
+        class="admin-custom-input-text"
         type="text"
         placeholder="Colour name"
         v-model="color.colour_name"
-         @input="addColorIfNeeded"
       />
-      <button v-if="modelValue.product_colors" @click.prevent="removeColor(index)">🗑️</button>
+      <button 
+      class="admin-svg-button"
+        v-if="modelValue.product_colors" 
+        @click.prevent="removeColor(index)"
+      >
+      <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"/></svg>
+      </button>
     </div>
- 
+    <div class="admin-form-item__create-color">
+      <input 
+      class="admin-custom-input-color"
+        type="color" 
+        v-model="newColor.hex_value"
+      >
+      <input
+        class="admin-custom-input-text"
+        type="text"
+        placeholder="Hex value"
+        v-model="newColor.hex_value"
+      />
+      <span class="admin-form-item__hex" :style="{ backgroundColor: newColor.hex_value }"></span>
+      <input
+        class="admin-custom-input-text"
+        type="text"
+        placeholder="Colour name"
+        v-model="newColor.colour_name"
+      />
+      <button 
+        class="admin-svg-button"
+        @click="addColor"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
+    </button>
   </div>
+</div>
 </template>
 
 <script setup lang="ts">
-import { computed} from 'vue';
+//vue
+import { reactive } from 'vue';
 
-interface ProductColor {
+interface ProductColor{
   hex_value: string;
   colour_name: string;
 }
 
-interface ProductColorTab {
-  product_colors?: ProductColor[];
+interface ProductColorTab{
+  product_colors: ProductColor[]
 }
 
-const productColors = computed({
-  get: () => modelValue.value.product_colors ?? [],
-  set: (val) => { modelValue.value.product_colors = val }
-});
+const modelValue = defineModel<ProductColorTab>({default: () => ({product_colos: []})})
 
-// props / model
-const modelValue = defineModel<ProductColorTab>({ default: () => ({
-  product_colors: [
-    { hex_value: '', colour_name: '' }
-  ]
-}) });
-
-// methods
-function addColorIfNeeded() {
-    const colors = modelValue.value.product_colors ?? [];
-  const last = colors[colors.length - 1];
-
-  if (!last?.hex_value && !last?.colour_name) return;
-
-  colors.push({ hex_value: '', colour_name: '' });
+//remove color
+function removeColor(index: number){
+  modelValue.value.product_colors.splice(index, 1);
 }
 
-function removeColor(index: number) {
-  productColors.value.splice(index, 1);
+//new color
+const newColor = reactive<ProductColor>({
+  hex_value: '#ffffff',
+  colour_name: ''
+})
+
+//add new color
+function addColor(){
+  if(!newColor.colour_name.trim()) return;
+
+    modelValue.value.product_colors.push({
+      hex_value: newColor.hex_value,
+      colour_name: newColor.colour_name
+    });
+
+    newColor.hex_value = '#121212';
+    newColor.colour_name = ''
+
 }
 </script>
 
-<style scoped>
-.color-row {
-  display: flex;
-  gap: 0.5rem;
-  align-items: center;
-  margin-bottom: 0.5rem;
-}
-.color-picker {
-  width: 2rem;
-  height: 2rem;
-  border: none;
-}
-.remove-btn {
-  color: red;
-  background: none;
-  border: none;
-  font-size: 1.2rem;
-  cursor: pointer;
-}
-.add-btn {
-  margin-top: 1rem;
-  background-color: #eee;
-  padding: 0.5rem 1rem;
-  cursor: pointer;
-}
-.circle{
-  width: 30px;
-  height: 30px;
-}
-
-</style>

e1810031ef38453ce9c9dac86f6f21ec514b8526
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-11-12 16:29:12 +0200

    feat(Skeletons): create ProductCardSkeleton, and ProductDetailsSkeleton components

---
+++ frontend/src/components/skeletons/ProductCardSkeleton.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="product-card-skeleton">
+   <div class="product-card-skeleton__img shimmer" >
+   </div>
+   <div class="product-card-skeleton__content">
+      <div class="product-card-skeleton__title shimmer"></div>
+      <div class="product-card-skeleton__info">
+         <div class="product-card-skeleton__text shimmer"></div>
+         <div class="product-card-skeleton__text shimmer"></div>
+         <div class="product-card-skeleton__text shimmer"></div>
+      </div>
+   </div>
+</div>
+</template>
+
+<script setup lang="ts">
+//vue 
+//Component settings
+defineOptions({
+  name: 'ProductCardSkeleton'
+})
+</script>
---
+++ frontend/src/components/skeletons/ProductDetailsSkeleton.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="product-details-skeleton">
+   <div class="product-details-skeleton__layout">
+      <div class="product-details-skeleton__details">
+         <div class="product-details-skeleton__details--top">
+            <div class="product-details-skeleton__details--text shimmer"></div>
+            <div class="product-details-skeleton__details--text shimmer"></div>
+         </div>
+         <div class="product-details-skeleton__details--bottom  shimmer"></div>
+      </div>
+      <div class="product-details-skeleton__media shimmer">    
+      </div>
+      <div class="product-details-skeleton__info">
+         <div class="product-details-skeleton__info--price shimmer"></div>
+         <div class="product-details-skeleton__info--colors">
+            <div class="product-details-skeleton__info--color shimmer"></div>
+             <div class="product-details-skeleton__info--color shimmer"></div>
+              <div class="product-details-skeleton__info--color shimmer"></div>
+         </div>
+      </div>
+   </div>
+   <div class="product-details-skeleton__description">
+      <div class="product-details-skeleton__text shimmer"></div>
+      <div class="product-details-skeleton__text shimmer"></div>
+      <div class="product-details-skeleton__text shimmer"></div>
+      <div class="product-details-skeleton__text shimmer"></div>
+   </div>
+</div>
+</template>
+
+<script setup lang="ts">
+//vue 
+//Component settings
+defineOptions({
+  name: 'ProductDetailsSkeleton'
+})
+</script>

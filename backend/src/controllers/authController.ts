e27725c7c51f6f14e3671b9db01f7a2e5c7dabaa
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-11-19 18:28:57 +0200

    feat(authController): create register logic

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -28,3 +28,29 @@ export const login = (req: Request, res: Response) => {
    });
 
 }
+
+//register 
+export const register = (req: Request, res: Response) => {
+   const {username, password, email} = req.body;
+
+   const existingUser = users.find((u) => u.username === username || u.email === email);
+
+   if(existingUser) {
+     return res.status(400).json({ message: "This user exist!"})
+   }
+
+   const newUser = {
+      id: Date.now(),
+      username,
+      email,
+      password,
+      role: 'user' as "user"
+   }
+
+   users.push(newUser);
+   
+   console.log("REGISTER request received!");
+
+   res.status(201).json({message: "User was created!", user: newUser})
+
+}

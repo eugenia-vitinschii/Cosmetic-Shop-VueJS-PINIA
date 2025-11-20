d6617e66d5c5d682db3ef43eb1186dcf0b7a685f
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-11-20 18:02:11 +0200

    feat(generateToken): generate token using jwt

---
+++ backend/src/utils/generateToken.ts
@@ -0,0 +1,7 @@
+// generate token
+
+import jwt from "jsonwebtoken";
+
+export const generateToken =  (id: number, role: string) =>{
+   return jwt.sign( {id, role}, process.env.JWT_SECRET as string, { expiresIn: "7d"})
+}

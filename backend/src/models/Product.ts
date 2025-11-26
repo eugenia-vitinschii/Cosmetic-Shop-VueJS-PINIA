03d7a0573b7fc8b242ccbc549dd9828c3b93c829
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-11-26 18:28:07 +0200

    feat(Product.ts): create IProduct interface and ProductSchema

---
+++ backend/src/models/Product.ts
@@ -0,0 +1,64 @@
+//product ts
+
+import mongoose, { Schema, Document} from "mongoose";
+
+export interface ProductColor {
+   hex_value: string;
+   colour_name: string;
+}
+
+export interface IProduct extends Document {
+   brand: string;
+   name: string;
+   price: number;
+   price_sign: string;
+   currency: string;
+   image_link?: string;
+   product_link?: string;
+   website_link?: string;
+   description?: string;
+   rating?: number;
+   category: string;
+   product_type: string;
+   tag_list?: string[];
+   created_at?: string;
+   updated_at?: string;
+   product_api_url?: string;
+   api_featured_image?: string;
+   product_colors?: ProductColor[];
+   sliderTags?: string[];
+   selectedColor?: ProductColor;
+   colorKey?: string;
+}
+
+const ProductSchema: Schema = new Schema({
+   brand: {type: String, required: true },
+   name: {type: String, required: true },
+   price: {type: Number, required: true },
+   price_sign: {type: String, required: true },
+   currency: {type: String, required: true },
+   image_link: {type: String},
+   product_link: {type: String},
+   website_link: {type: String},
+   description: {type: String},
+   rating:  {type: Number},
+   category: {type: String, required: true },
+   product_type: {type: String, required: true },
+   tag_list: [{type: String}],
+   product_api_url: {type: String},
+   api_featured_image: {type: String},
+   product_colors: [
+      {
+         hex_value:{type: String},
+         colour_name: {type: String}
+      }
+   ],
+   sliderTags: [ {type: String}],
+   selectedColor:{
+      hex_value:{type: String},
+      colour_name: {type: String}
+   },
+   colorKey:  {type: String},
+}, {timestamps: true});
+
+export default mongoose.model<IProduct>("Product", ProductSchema);

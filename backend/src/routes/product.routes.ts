18395537914438fd5d0f5b3c60e017804dfbcd22
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-11-28 19:38:45 +0200

    (routes): create product routes, rename auth, admin routes

---
+++ backend/src/routes/product.routes.ts
@@ -0,0 +1,11 @@
+//product routes ts
+
+import { Router } from "express";
+import {getProducts, getProductsById} from "../controllers/product.controller";
+
+const router = Router();
+
+router.get("/", getProducts);
+router.get("/:id", getProductsById);
+
+export default router;

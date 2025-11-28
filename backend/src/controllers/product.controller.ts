997fb097ab7fa5cb9f2694900fb4e652313902c3
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-11-28 19:36:49 +0200

    feat(product.controller): move pinia store logic: getProducts, getProductById

---
+++ backend/src/controllers/product.controller.ts
@@ -0,0 +1,55 @@
+// product.controller.ts
+
+import Product from "../models/product.model";
+import { Request, Response} from "express";
+
+/* === getProducts === */
+
+export const getProducts  = async(req: Request, res: Response) =>{
+   try{
+      const{
+         page = 1,
+         limit = 30,
+         brand, type,category, tag
+      } = req.query;
+   
+      const query: any = {};
+
+      if(brand) query.brand = brand;
+      if(type) query.type= type;
+      if(category) query.category = category;
+      if(tag) query.tag = tag;
+
+      const skip = (Number(page) - 1) * Number(limit);
+
+      const products = await Product.find(query).skip(skip).limit(Number(limit));
+
+      const count = await Product.countDocuments(query);
+
+      res.json({
+         item: products,
+         total:count,
+         page: Number(page),
+         limit: Number(limit),
+         complected: products.length < Number(limit),
+      });
+   }catch(err) {
+      console.log("GetProducts error:", err);
+      res.status(500).json({message: "Server error"})
+   }
+};
+
+/* === getProductsById === */
+
+export  const getProductsById = async (req:Request, res: Response) =>{
+   try{
+      const product = await Product.findById(req.params.id);
+
+      if(!product) return res.status(404).json({message: "Not found"});
+
+      res.json(product)
+   }catch(error){
+      console.log("getProductById error:", error);
+      res.status(500).json({message: "Server Error"})
+   }
+}

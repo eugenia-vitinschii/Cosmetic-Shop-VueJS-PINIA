d8539c40569713b2f510b2fac146f9305c4488dc
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-11-29 17:05:51 +0200

    refactor(middleware): renamed, and delete unused logic

---
+++ backend/src/middleware/admin.middleware.ts
@@ -0,0 +1,16 @@
+// // adminMiddleware.ts
+
+import { Request, Response, NextFunction } from "express";
+
+export const adminMiddleware = (req:Request, res: Response, next: NextFunction) => {
+
+   if(!req.user ){
+      return res.status(401).json({message: "Not authenticated"})
+   }
+
+   if(req.user.role  !== "admin"){
+      return res.status(403).json({ message: "Acces denied"})
+   }
+   next();
+}
+

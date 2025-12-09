18faec545db32d9453857b40865e87bae4697a40
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-12-09 18:29:50 +0200

    refactor(product.routes): implement validatesource middleware and updated product controllers

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -1,11 +1,23 @@
 //product routes ts
 
 import { Router } from "express";
-import {getProducts, getProductsById} from "../controllers/product.controller";
+import {productController} from "../controllers/product.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
+import { adminMiddleware } from "../middleware/admin.middleware";
+import { validateresource } from "../middleware/validateResource";
+import { ProductSchema } from "../dtos/product.dto";
 
 const router = Router();
 
-router.get("/", getProducts);
-router.get("/:id", getProductsById);
+/* === PUBLIC ROUTES === */
+router.get("/", productController.getAll);
+router.get("/:id", productController.getById);
+
+/* === ADMIN ONLY ROUTES === */
+router.post("/", authMiddleware, adminMiddleware, validateresource(ProductSchema), productController.create );
+
+router.put('/:id',authMiddleware, adminMiddleware, validateresource(ProductSchema), productController.update);
+
+router.delete("/:id", authMiddleware, adminMiddleware, productController.delete);
 
 export default router;

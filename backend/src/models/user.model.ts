ffc8297015671eff1243b932417370378e10d654
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-12-10 14:53:27 +0200

    fear(user.model): create pre-save hook: if the password filed i snew or has been changed generate a salt and hash the password

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,6 +1,7 @@
 //user model
 
 import mongoose, {Schema, Document} from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document {
    username: string;
@@ -10,12 +11,20 @@ export interface IUser extends Document {
 }
 
 
-const UserSchema: Schema = new Schema ({
+const UserSchema: Schema = new Schema<IUser> ({
    username: {type: String, required: true, unique: true},
    email: {type: String, required: true, unique: true},
    password: { type: String, required: true},
    role: { type: String, enum: ["admin", "user"], default: "user"}
 }, {timestamps: true});
 
+UserSchema.pre<IUser>("save", async function (next){
+   if (!this.isModified("password")) return next();
+
+   const salt = await bcrypt.genSalt(10);
+   this.password = await bcrypt.hash(this.password, salt);
+   next()
+
+})
 
 export default mongoose.model<IUser>("User", UserSchema);

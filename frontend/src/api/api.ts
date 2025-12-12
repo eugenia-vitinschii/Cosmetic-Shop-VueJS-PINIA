1cbdeb80b93b0506360af294e923d406ed47194e
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-12-12 17:35:31 +0200

    feat(api.ts): create axios instance to add token automatically

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,21 @@
+//axios instance
+
+import axios from "axios";
+import { useAuthStore } from "@/stores/auth.store";
+
+const api = axios.create({
+   baseURL: "http://localhost:4000/api",
+});
+
+//interceptor
+api.interceptors.request.use((config) =>{
+    const auth = useAuthStore();
+
+    if(auth.token){
+      config.headers.Authorization = `Bearer ${auth.token}`;
+    }   
+   
+    return config;
+});
+
+export default api;

521a41124b35e1251c7b508a95a7aa20e77443b4
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-12-19 17:08:50 +0200

    feat(UsersDashboard): implement delete logic from store, add link to edit user view

--- frontend/src/views/admin/UsersDashboardView.vue
+++ frontend/src/views/admin/UsersDashboardView.vue
@@ -24,14 +24,17 @@
                      <td class="admin-body-text">{{ user.email }}</td>
                      <td class="admin-body-text">{{user.role}}</td>
                      <td>
-                        <button class="admin-svg-button">
+                        <button 
+                           @click="deleteUser(user.id)"
+                           class="admin-svg-button"
+                           >
                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" /></svg>
                         </button>
                      </td>
                      <td>
-                        <button class="admin-svg-button">
+                        <router-link :to="'/admin/user/' + user.id" class="admin-svg-button">
                            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" /> </svg>
-                        </button>
+                        </router-link>
                      </td>
                   </tr>
                </tbody>
@@ -54,6 +57,7 @@ import AdminTitle from '@/components/admin/ui/AdminTitle.vue';
 
 //pinia
 import { useAdminUserStore } from '@/stores/admin.users.store';
+import { onMounted } from 'vue';
 
 //Component settings
 defineOptions({
@@ -63,4 +67,19 @@ defineOptions({
 
 //variables
 const adminUser = useAdminUserStore();
+
+//functions
+
+const deleteUser = (id?: string) => {
+  if (!id) return;
+  const confirm = window.confirm("delete User?");
+  if (confirm) {
+    adminUser.deleteUser(id);
+  }
+}
+
+//hooks
+onMounted(() => {
+   adminUser.fetchUsers();
+})
 </script>

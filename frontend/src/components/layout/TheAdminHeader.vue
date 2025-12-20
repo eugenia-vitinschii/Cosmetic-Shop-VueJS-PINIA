58aebc2a9cc5e53dce0648dd1dedb62146645d91
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-12-20 16:15:30 +0200

    feat(AdminHeader): add auth-aware header

--- frontend/src/components/layout/TheAdminHeader.vue
+++ frontend/src/components/layout/TheAdminHeader.vue
@@ -4,7 +4,11 @@
          <button class="admin-svg-button" @click="isSidebarOpen = !isSidebarOpen">
             <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm280-80h280v-560H480v560Z"/></svg>
          </button>
-         <p class="admin-subheading">Hi, admin!</p>
+         <p 
+            class="admin-subheading"
+            v-if="auth.user"
+         >Hi, {{auth.user?.username}}!
+         </p>
          <admin-theme-toggle/>
       </div>
       <admin-sidebar 
@@ -19,6 +23,8 @@
 //vue
 import {ref} from 'vue'
 
+//stores
+import { useAuthStore } from '@/stores/auth.store';
 //components
 import AdminThemeToggle from '../admin/ui/AdminThemeToggle.vue';
 import AdminSidebar from './AdminSidebar.vue';
@@ -31,4 +37,6 @@ defineOptions({
 //variables
 const isSidebarOpen = ref(false)
 
+//pinia
+const auth = useAuthStore()
 </script>

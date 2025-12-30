ce741dd897b7675fc742b208e35b347d788dcf88
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-12-30 16:25:15 +0200

    feat(AdminInput): add error message, class and sass rules fow show error input

--- frontend/src/components/admin/ui/AdminInput.vue
+++ frontend/src/components/admin/ui/AdminInput.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class="admin-input__wrapper">
+    <div 
+      :class="{'admin-input-error__wrapper': error}"
+      class="admin-input__wrapper"
+    >
       <label class="admin-body-text" :class="{'admin-disabled-text': disabled}">{{ label }}</label>
       <input
          class="admin-custom-input-text"
+         
          type="text"
          :value="modelValue"
          :disabled="disabled"
          @input="onInput"
          :placeholder="placeholder"
       />
+      <p class="body-text red" v-if="error">{{ error }}</p>
   </div>
 </template>
 
@@ -24,7 +29,8 @@ defineOptions({
 const props = defineProps<{
   modelValue: number | string | undefined,
   label: string,
-  placeholder?: string
+  placeholder?: string,
+  error?: string,
   disabled?: boolean
 }>()
 
